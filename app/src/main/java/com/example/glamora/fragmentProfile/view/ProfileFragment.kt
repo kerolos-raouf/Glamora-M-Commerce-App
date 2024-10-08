ac365bc951eec2bea52deeed55179c051a0e8438
Mai Ibrahim <89744840+MaiBayoumi@users.noreply.github.com>
2024-10-08 13:03:23 +0300

    handel guest mood in orders

--- app/src/main/java/com/example/glamora/fragmentProfile/view/ProfileFragment.kt
+++ app/src/main/java/com/example/glamora/fragmentProfile/view/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.glamora.fragmentProfile.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -44,9 +45,13 @@ class ProfileFragment : Fragment() {
             communicator.hideBottomNav()
             findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
         }
-        binding.profileOrdersIcon.setOnClickListener {
-            communicator.hideBottomNav()
-            findNavController().navigate((R.id.action_profileFragment_to_ordersFragment))
+        binding.profileOrdersLayout.setOnClickListener {
+            if (sharedViewModel.currentCustomerInfo.value.displayName == Constants.UNKNOWN) {
+                showSignInDialog()
+            } else {
+                communicator.hideBottomNav()
+                findNavController().navigate(R.id.action_profileFragment_to_ordersFragment)
+            }
         }
         if(sharedViewModel.currentCustomerInfo.value.displayName != Constants.UNKNOWN)
             binding.profileUsername.text = "Hello, ${sharedViewModel.currentCustomerInfo.value.displayName}"
@@ -55,6 +60,19 @@ class ProfileFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         communicator.showBottomNav()
+    }
+
+    private fun showSignInDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage("Please sign in to view your orders.")
+            .setPositiveButton("Sign In") { dialog, _ ->
+                findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 
 }
