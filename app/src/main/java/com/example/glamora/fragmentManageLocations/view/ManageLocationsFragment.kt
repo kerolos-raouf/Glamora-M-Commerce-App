929493253bf0d189b106dabb0c97625b186e2646
kerolos-raouf <83724226+kerolos-raouf@users.noreply.github.com>
2024-10-09 20:24:38 +0300

    ability to remove unwanted address

--- app/src/main/java/com/example/glamora/fragmentManageLocations/view/ManageLocationsFragment.kt
+++ app/src/main/java/com/example/glamora/fragmentManageLocations/view/ManageLocationsFragment.kt
@@ -8,17 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.glamora.R
 import com.example.glamora.databinding.FragmentManageLocationsBinding
+import com.example.glamora.fragmentManageLocations.viewModel.ManageAddressesViewModel
 import com.example.glamora.mainActivity.viewModel.SharedViewModel
 import com.example.glamora.util.customAlertDialog.CustomAlertDialog
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ManageLocationsFragment : Fragment() {
 
     //view models
     private val sharedViewModel : SharedViewModel by activityViewModels()
+    private val manageAddressesViewModel : ManageAddressesViewModel by viewModels()
 
     private lateinit var binding : FragmentManageLocationsBinding
 
@@ -32,25 +39,29 @@ class ManageLocationsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_manage_locations, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = manageAddressesViewModel
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        initObservers()
     }
 
 
     private fun initViews(){
         customAlertDialog = CustomAlertDialog(requireActivity())
-        adapter = ManageLocationsRecyclerAdapter{
+        adapter = ManageLocationsRecyclerAdapter{address->
             customAlertDialog.showAlertDialog(
                 "Are you sure you want to delete this address?",
                 "Delete"
             ){
-
+                val newAddresses = sharedViewModel.currentCustomerInfo.value.addresses.filter { it != address}
+                manageAddressesViewModel.deleteCustomerAddresses(sharedViewModel.currentCustomerInfo.value.userId,newAddresses)
             }
         }
         binding.addressesRecyclerView.adapter = adapter
@@ -60,6 +71,21 @@ class ManageLocationsFragment : Fragment() {
             adapter.submitList(sharedViewModel.currentCustomerInfo.value.addresses)
         }
 
+    }
+
+
+    private fun initObservers(){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED)
+            {
+                manageAddressesViewModel.customerAddresses.collect{addresses->
+                    addresses?.let {
+                        adapter.submitList(it)
+                        sharedViewModel.currentCustomerInfo.value.addresses = it
+                    }
+                }
+            }
+        }
     }
 
 
